10c8ff51590668f48d40b146b607951cadaf2e0c
Carlos Hanson <carlos@clanhanson.com>
2020-03-06 02:17:04 +0000

    Added URL to GitHub.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='bottle-pycouchdb',
         version='0.1',
         description='Add support for CouchDB to Bottle using pycouchdb',
-        url='',
+        url='https://github.com/carloshanson/bottle-pycouchdb',
         author='Carlos Hanson',
         author_email='carlos@clanhanson.com',
         license='MIT',
